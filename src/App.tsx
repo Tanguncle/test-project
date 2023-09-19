0d407eeab950385d6d9aa7a8c935574c74c10df1
tangUncle <manson@LeeMacBook-Pro.local>
2023-09-20 01:45:39 +0800

    feat: finished copyToClipboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button } from 'antd'
 import './App.css';
+import { CopyToClipboard } from './components/CopyToClipboard';
 
 function App() {
   return (
     <div className="App">
-      WelCome To My First CreatApp
-      <Button type='primary'>复制</Button>
+      <CopyToClipboard buildCopyData={()=>'123'}>
+      <Button type="primary">复制--把内容放进剪贴板</Button>
+      </CopyToClipboard>
     </div>
   );
 }
--- src/components/CopyToClipboard/index.tsx
+++ src/components/CopyToClipboard/index.tsx
@@ -1,67 +1,78 @@
-import React, { useEffect, useRef } from 'react';
-import { CopyOutlined } from '@ant-design/icons';
-import { Button, message } from 'antd';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef } from "react";
+import { CopyOutlined } from "@ant-design/icons";
+import { Button, message } from "antd";
 
-export const CopyToClipboard: React.FC<React.PropsWithChildren<{
+export const CopyToClipboard: React.FC<
+  React.PropsWithChildren<{
     buildCopyData: () => string | Promise<String>;
     disabled?: boolean;
     isshowIcon?: boolean;
     title?: string | React.ReactNode;
-  }>> = (props) => {
-  const { isshowIcon = true, title = '复制' } = props;
+  }>
+> = (props) => {
+  const { isshowIcon = true, title = "复制" } = props;
   const isThisComp = useRef(false);
 
-
-  const pasteDataToClipboard = (event: any, clipboardData: any, data: string) => {
-    clipboardData.setData('text/plain', data);
+  const pasteDataToClipboard = (
+    event: any,
+    clipboardData: any,
+    data: string
+  ) => {
+    clipboardData.setData("text/plain", data);
     message.destroy();
     event.preventDefault();
-    message.success('复制成功');
+    message.success("复制成功");
     isThisComp.current = false;
   };
   useEffect(() => {
-    const onCopy = async (event:any) => {
+    const onCopy = async (event: any) => {
       if (!isThisComp.current) return;
 
       if (event.clipboardData || event.originalEvent) {
-        message.loading('处理数据中...');
-        const clipboardData = (event.clipboardData || event.originalEvent.clipboardData);
+        message.loading("处理数据中...");
+        const clipboardData =
+          event.clipboardData || event.originalEvent.clipboardData;
         const copyData = props.buildCopyData();
-        if (typeof copyData === 'object') {
+        if (typeof copyData === "object") {
           const res = await copyData;
           pasteDataToClipboard(event, clipboardData, res as string);
           // copyData.then(res => {
           //   pasteDataToClipboard(event, clipboardData, res as string);
           // });
         }
-        if (typeof copyData === 'string') { pasteDataToClipboard(event, clipboardData, copyData as string); }
+        if (typeof copyData === "string") {
+          pasteDataToClipboard(event, clipboardData, copyData as string);
+        }
       }
     };
 
-    document.addEventListener('copy', onCopy);
+    document.addEventListener("copy", onCopy);
 
     return () => {
-      document.removeEventListener('copy', onCopy);
+      document.removeEventListener("copy", onCopy);
     };
   }, [props.buildCopyData]);
-  return props.children
-    ? (
-      <div
-        onClick={() => {
-          isThisComp.current = true;
-          document.execCommand('copy', false, '123');
-        }}
-      >{ props.children }
-      </div>)
-    : (
-      <Button
-        type="link"
-        disabled={props.disabled}
-        icon={isshowIcon ? <CopyOutlined /> : null}
-        onClick={() => {
-          isThisComp.current = true;
-          document.execCommand('copy', false, '123');
-        }}
-      >{title}
-      </Button>);
+  return props.children ? (
+    <div
+      onClick={() => {
+        isThisComp.current = true;
+        document.execCommand("copy", false, "123");
+      }}
+    >
+      {props.children}
+    </div>
+  ) : (
+    <Button
+      type="link"
+      disabled={props.disabled}
+      icon={isshowIcon ? <CopyOutlined /> : null}
+      onClick={() => {
+        isThisComp.current = true;
+        document.execCommand("copy", false, "123");
+      }}
+    >
+      {title}
+    </Button>
+  );
 };
