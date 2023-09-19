5d9db36a6c6726bb10793a7cbfdf421680284147
tangUncle <manson@LeeMacBook-Pro.local>
2023-09-19 23:40:19 +0800

    feat: 新增复制能力组建

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'antd'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       WelCome To My First CreatApp
+      <Button type='primary'>复制</Button>
     </div>
   );
 }
